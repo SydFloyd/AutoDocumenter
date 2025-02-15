71ade6705c4a12779ffb8525b5da1afd464a4138
= <=>
2025-02-15 12:28:39 -0500

    remove debugss

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-echo "Current directory: $(pwd)"
-
-if [ ! -f "/app/entrypoint.sh" ]; then
-    echo "Error: entrypoint.sh is missing!"
-    exit 1
-fi
-
-echo "Python version:"
-python --version || echo "Python is missing!"
-
 echo "Running script with arguments: $@"
 python /app/AutoDocstring.py "$@"
-# PYTHONUNBUFFERED=1 python /app/AutoDocstring.py "$@" | tee /dev/tty
 echo "Script execution completed."
