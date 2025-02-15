5497ff19a39ae64c4857bd02d5a6ca03b23f394c
= <=>
2025-02-15 12:45:27 -0500

    back to b4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-# Debugging
-echo "Raw input arguments: $@"
-
-# Convert newline-separated input into an array
-IFS=$'\n' read -r -d '' -a FILE_ARRAY <<< "$(printf "%s" "$@")"
-
-echo "Running script with arguments: ${FILE_ARRAY[@]}"
-python /app/AutoDocstring.py "${FILE_ARRAY[@]}"
+echo "Running script with arguments: $@"
+python /app/AutoDocstring.py "$@"
 echo "Script execution completed."
